49affb3cdeb3f510151bccf91364dfb74ac28344
Cleverson <SmileYzn@users.noreply.github.com>
2026-09-21 15:23:57 -0300

    Update user directory paths to use whoami

--- xfce.sh
+++ xfce.sh
@@ -3,9 +3,6 @@
 # Limpar
 clear
 
-# Usuário padrão (UID 1000)
-USUARIO=$(id -nu 1000)
-
 # Verificar acesso root
 if [[ $EUID -eq 0 ]]; then
     echo -e "Esse script NÃO deve ser executado como ${USER}"
@@ -13,7 +10,7 @@ if [[ $EUID -eq 0 ]]; then
 fi
 
 # Abrir pasta do usuário
-cd /home/$USUARIO
+cd /home/$(whoami)
 
 # Atualizar XBPS
 sudo xbps-install -Syu xbps
@@ -31,52 +28,121 @@ sudo xbps-install -Syu
 sudo xbps-install -Syu void-repo-nonfree
 
 # Pacotes base
-sudo xbps-install -Syu 7zip 7zip-unrar alsa-firmware alsa-utils bash-completion blueman fastfetch ffmpeg ffmpegthumbnailer git man nano pipewire power-profiles-daemon powertop system-config-printer unzip unrar xz zip
+sudo xbps-install -Syu \
+7zip \
+7zip-unrar \
+alsa-firmware \
+alsa-utils \
+bash-completion \
+blueman \
+bluez \
+fastfetch \
+ffmpeg \
+ffmpegthumbnailer \
+git \
+man \
+nano \
+numlockx \
+pipewire \
+power-profiles-daemon \
+powertop \
+system-config-printer \
+unzip \
+unrar \
+xiccd \
+xorg-apps \
+xz \
+zip
 
-# Adwaita
-sudo xbps-install -Syu adwaita-fonts adwaita-icon-theme
-
-# Fontes Adobe
-sudo xbps-install -Syu font-adobe-source-code-pro font-adobe-source-serif-pro
-
-# Fontes Noto Sans
-sudo xbps-install -Syu noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf noto-fonts-ttf-extra noto-fonts-ttf-variable
-
-# Fontes Fira Sans, Fira Code
-sudo xbps-install -Syu font-fira-otf font-fira-ttf font-firacode
-
-# Fontes Droid Sans, Open Sans, Roboto, ubuntu
-sudo xbps-install -Syu fonts-droid-ttf ttf-opensans fonts-roboto-ttf ttf-ubuntu-font-family
+# Fontes 
+sudo xbps-install -Syu \
+adwaita-fonts \
+dejavu-fonts-ttf \
+font-adobe-source-code-pro \
+font-adobe-source-serif-pro \
+fonts-droid-ttf \
+font-fira-otf \
+font-fira-ttf \
+font-firacode \
+font-inconsolata-otf \
+fonts-roboto-ttf \
+noto-fonts-cjk \
+noto-fonts-emoji \
+noto-fonts-ttf \
+noto-fonts-ttf-extra \
+noto-fonts-ttf-variable \
+ttf-opensans \
+ttf-ubuntu-font-family
 
 # Atualizar cache de fontes
 sudo fc-cache -f -v
 
 # XDG Utils
-sudo xbps-install -Syu xdg-user-dirs xdg-user-dirs-gtk xdg-desktop-portal xdg-desktop-portal-gtk xdg-utils
-
-# XORG
-sudo xbps-install -Syu numlockx xiccd xorg-apps
+sudo xbps-install -Syu \
+xdg-user-dirs \
+xdg-user-dirs-gtk \
+xdg-desktop-portal \
+xdg-desktop-portal-gtk \
+xdg-utils
 
 # NTFS, CIFS, GVFS
-sudo xbps-install -Syu cifs-utils ntfs-3g exfat-utils gvfs gvfs-afc gvfs-afp gvfs-cdda gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-smb
+sudo xbps-install -Syu \
+cifs-utils \
+ntfs-3g \
+exfat-utils \
+gvfs \
+gvfs-afc \
+gvfs-afp \
+gvfs-cdda \
+gvfs-goa \
+gvfs-gphoto2 \
+gvfs-mtp \
+gvfs-smb
 
 # XFCE4
-sudo xbps-install -Syu xfce4-plugins xfce4-alsa-plugin xfce4-docklike-plugin xfce4-eyes-plugin xfce4-genmon-plugin xfce4-panel-profiles xfce4-pulseaudio-plugin xfce4-screenshooter
+sudo xbps-install -Syu \
+xfce4-plugins \
+xfce4-alsa-plugin \
+xfce4-docklike-plugin \
+xfce4-eyes-plugin \
+xfce4-genmon-plugin \
+xfce4-panel-profiles \
+xfce4-pulseaudio-plugin \
+xfce4-screenshooter
 
 # Thunar
-sudo xbps-install -Syu thunar-archive-plugin thunar-media-tags-plugin thunar-volman
-
-# Firefox
-sudo xbps-install -Syu firefox firefox-i18n-pt-BR
+sudo xbps-install -Syu \
+thunar-archive-plugin \
+thunar-media-tags-plugin \
+thunar-volman
 
 # GStreamers
-sudo xbps-install -Syu gstreamer1 gst-libav gst-plugins-bad1 gst-plugins-base1 gst-plugins-good1 gst-plugins-ugly1
+sudo xbps-install -Syu \
+gstreamer1 \
+gst-libav \
+gst-plugins-bad1 \
+gst-plugins-base1 \
+gst-plugins-good1 \
+gst-plugins-ugly1
 
 # Extras
-sudo xbps-install -Syu catfish dconf-editor galculator gcolor3 gparted gthumb lightdm-gtk-greeter-settings mugshot orage parole peek seahorse simple-scan xarchiver zeitgeist
-
-# Bluetoth, CUPS
-sudo xbps-install -Syu blueman bluez cups libspa-bluetooth
+sudo xbps-install -Syu \
+catfish \
+dconf-editor \
+firefox \
+firefox-i18n-pt-BR \
+galculator \
+gcolor3 \
+gparted \
+gthumb \
+lightdm-gtk-greeter-settings \
+mugshot \
+orage \
+parole \
+peek \
+seahorse \
+simple-scan \
+xarchiver
 
 # Habilitar Bluetoth
 sudo rfkill unblock bluetooth
@@ -95,32 +161,36 @@ sudo xbps-remove -foy
 sudo groupadd -r autologin
 
 # Adicionar o usuário ao grupo
-sudo gpasswd autologin -a ${USUARIO}
+sudo gpasswd autologin -a $(whoami)
 
 # Abrir pasta do usuário
-cd /home/$USUARIO
+cd /home/$(whoami)
 
 # Criar pastas padrão
 xdg-user-dirs-update
 
 # Criar pastas
-mkdir Desktop Downloads Modelos Rede Documentos Músicas Imagens Vídeos
+mkdir Desktop Documentos Downloads Imagens Modelos Músicas Projetos Rede Vídeos
 
 # Alterar pastas
-xdg-user-dirs-update --force --set DESKTOP /home/$USUARIO/Desktop
-xdg-user-dirs-update --force --set DOWNLOAD /home/$USUARIO/Downloads
-xdg-user-dirs-update --force --set TEMPLATES /home/$USUARIO/Modelos
-xdg-user-dirs-update --force --set PUBLICSHARE /home/$USUARIO/Rede
-xdg-user-dirs-update --force --set DOCUMENTS /home/$USUARIO/Documentos
-xdg-user-dirs-update --force --set MUSIC /home/$USUARIO/Músicas
-xdg-user-dirs-update --force --set PICTURES /home/$USUARIO/Imagens
-xdg-user-dirs-update --force --set VIDEOS /home/$USUARIO/Vídeos
+xdg-user-dirs-update --force --set DESKTOP /home/$(whoami)/Desktop
+xdg-user-dirs-update --force --set DOCUMENTS /home/$(whoami)/Documentos
+xdg-user-dirs-update --force --set DOWNLOAD /home/$(whoami)/Downloads
+xdg-user-dirs-update --force --set PICTURES /home/$(whoami)/Imagens
+xdg-user-dirs-update --force --set TEMPLATES /home/$(whoami)/Modelos
+xdg-user-dirs-update --force --set MUSIC /home/$(whoami)/Músicas
+xdg-user-dirs-update --force --set PROJECTS /home/$(whoami)/Projetos
+xdg-user-dirs-update --force --set PUBLICSHARE /home/$(whoami)/Rede
+xdg-user-dirs-update --force --set VIDEOS /home/$(whoami)/Vídeos
 
 # Atualizar pastas padrão
 xdg-user-dirs-update
 
 # Remover pastas antigas
-rm -rf Documents Music Pictures Public Templates Videos
+rm -rf Documents Music Pictures Projects Public Templates Videos Projects
+
+# Limpar histórico
+history -c && > ~/.bash_history
 
 # Fim
 exit
